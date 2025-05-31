43752a6f443fb0b52eb2bca7272c9ed069325e91
pricepsych <dev.pricepsych@gmail.com>
2025-05-31 02:51:14 +0200

    Delete pages directory

--- pages/api/adjust.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  try {
-    console.log("Simulerer prisjustering (charm pricing)...");
-    res.status(200).send("Prices adjusted successfully.");
-  } catch (error) {
-    console.error("Error adjusting prices:", error);
-    res.status(500).send("Error adjusting prices.");
-  }
-}
